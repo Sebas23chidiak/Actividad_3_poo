fba7221f55c0370702453c44131f2e5c4cdc0196
Sebas23chidiak <143117455+Sebas23chidiak@users.noreply.github.com>
2023-09-19 17:25:38 -0500

    Create Cuadrado.java

---
+++ Parte_2.docs/Cuadrado.java
@@ -0,0 +1,18 @@
+package geome_figuras;
+
+public class Cuadrado {
+    double lado;
+
+    public Cuadrado(double lado) {
+        this.lado = lado;
+    }
+
+    public double calcularArea() {
+        return lado * lado;
+    }
+
+    public double calcularPerimetro() {
+        return 4 * lado;
+    }
+}
+
