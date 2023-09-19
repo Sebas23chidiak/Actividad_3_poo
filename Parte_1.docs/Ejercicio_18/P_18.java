542adbbb7fc0d4cc44cff992ba691408eb6e6865
Sebas23chidiak <143117455+Sebas23chidiak@users.noreply.github.com>
2023-09-19 16:52:53 -0500

    Create P_18.java

---
+++ Parte_1.docs/Ejercicio_18/P_18.java
@@ -0,0 +1,8 @@
+package P_18;
+
+public class P18 {
+    public static void main(String[] args) {
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setVisible(true);
+    }
+}
