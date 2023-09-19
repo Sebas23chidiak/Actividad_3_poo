daeb44350d22123e8abb7333820507624d6f7e63
Sebas23chidiak <143117455+Sebas23chidiak@users.noreply.github.com>
2023-09-19 17:24:41 -0500

    Create Figu_Geometicas.java

---
+++ Parte_2.docs/Figu_Geometicas.java
@@ -0,0 +1,8 @@
+package geome_figuras;
+
+public class Geome_Figuras {
+    public static void main(String[] args) {
+        InterfazPrincipal ventana = new InterfazPrincipal();
+        ventana.setVisible(true);
+    }
+}
