dcfaa30da17c3c5ed7054f10253ef86214bea70a
Sebas23chidiak <143117455+Sebas23chidiak@users.noreply.github.com>
2023-09-19 17:37:47 -0500

    Update Rectangulo.java

--- Parte_2.docs/Rectangulo.java
+++ Parte_2.docs/Rectangulo.java
@@ -1,35 +1,18 @@
 package geome_figuras;
 
-public class InterfazTriangulo extends javax.swing.JFrame {
-    public InterfazTriangulo() {
-        initComponents();
-        txtArea.setEditable(false);
-        txtPerimetro.setEditable(false);
-        txtHipotenusa.setEditable(false);
+public class Rectangulo {
+    double base, altura;
+
+    public Rectangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
     }
 
-    private void calcularActionPerformed(java.awt.event.ActionEvent evt) {
-        double area, perimetro, hipotenusa;
-        double base = Double.parseDouble(txtBase.getText());
-        double altura = Double.parseDouble(txtAltura.getText());
-        TrianguloRectangulo triangulo = new TrianguloRectangulo(base, altura);
-        area = triangulo.calcularArea();
-        perimetro = triangulo.calcularPerimetro();
-        hipotenusa = triangulo.calcularHipotenusa();
-        txtArea.setText(String.valueOf(area));
-        txtPerimetro.setText(String.valueOf(perimetro));
-        txtHipotenusa.setText(String.valueOf(hipotenusa));
+    public double calcularArea() {
+        return base * altura;
     }
 
-    private void limpiarActionPerformed(java.awt.event.ActionEvent evt) {
-        txtBase.setText("");
-        txtAltura.setText("");
-        txtArea.setText("");
-        txtPerimetro.setText("");
-        txtHipotenusa.setText("");
-    }
-
-    private void salirActionPerformed(java.awt.event.ActionEvent evt) {
-        dispose();
+    public double calcularPerimetro() {
+        return (2 * base) + (2 * altura);
     }
 }
